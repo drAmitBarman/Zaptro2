30fbdfce5c3505a44cda576d03fd4bcdede35e3d
Amit Barman <ab2762314@gmail.com>
2025-08-05 15:53:49 +0530

    second commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {MapPin} from 'lucide-react'
 
 const Navbar = () => {
+  const location=false
   return (
     <div className='bg-white py-3 shadow-2xl'>
       <div className='max-w-6xl mx-auto flex justify-between items-center'>
           {/* logo section */}
+          <div>
           <Link to={'/'}><h1 className='font-bold text-2xl'><span className='text-red-500 font-serif'>Z</span>aptro</h1></Link>
+          <div className='flex gap-1 cursor-pointer text-gray-700 items-center'>
+             <MapPin className='text-red-500'/>
+             <span className='font-semibold' >{location ? <div></div>:"Add Adress"}</span>
+             </div>
+          </div>
        </div>
 
     </div>
