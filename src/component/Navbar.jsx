7214dc313643915bbfa90ead9673db3a03b9ddf9
Amit Barman <ab2762314@gmail.com>
2025-08-08 19:27:57 +0530

    4th commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {MapPin} from 'lucide-react'
+import {FaCaretDown} from 'react-icons/fa'
 
 const Navbar = () => {
   const location=false
@@ -8,13 +9,17 @@ const Navbar = () => {
     <div className='bg-white py-3 shadow-2xl'>
       <div className='max-w-6xl mx-auto flex justify-between items-center'>
           {/* logo section */}
-          <div>
+          <div className='flex gap-7 items-center'>
           <Link to={'/'}><h1 className='font-bold text-2xl'><span className='text-red-500 font-serif'>Z</span>aptro</h1></Link>
           <div className='flex gap-1 cursor-pointer text-gray-700 items-center'>
              <MapPin className='text-red-500'/>
              <span className='font-semibold' >{location ? <div></div>:"Add Adress"}</span>
+
+             <FaCaretDown/>
              </div>
           </div>
+          {/* manue sating */}
+          
        </div>
 
     </div>
